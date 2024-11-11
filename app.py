43d07711c4f54632af9e9ddd80608403ece15406
Egemen Öztürk <66872413+egemnoztrk@users.noreply.github.com>
2024-11-12 00:21:40 +0300

    web deployment

--- app.py
+++ app.py
@@ -3,18 +3,13 @@ from dotenv import load_dotenv
 from routes import FlaskApp
 import os
 
-# Load environment variables at the module level
-load_dotenv(override=True)
-flask_secret_key = os.getenv("FLASK_SECRET_KEY", "")
-mysql_uri = os.getenv("MYSQL_URI", "")
-
-# Create the Flask app instance
-flask_app = FlaskApp(flask_secret_key, mysql_uri)
-
 if __name__ == '__main__':
-    # Run the Flask development server
+    load_dotenv(override=True)
+    
+    flask_secret_key = os.getenv("FLASK_SECRET_KEY", "")
+    mysql_uri = os.getenv("MYSQL_URI", "")
+    flask_app = FlaskApp(flask_secret_key, mysql_uri)
     flask_app.run()
     
-    # Keep the process alive (optional)
     while True:
         time.sleep(60)
